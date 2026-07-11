b8790722ad07e2b6511955718fbff30de446476f
Dmitry <hermes@8574613-cn065576.twc1.net>
2026-07-11 17:32:29 +0000

    Revert "fix: skip works without images in gallery"
    This reverts commit 78a7bb39d260268d56906fb0c3b1941b112762b4.

--- js/script.js
+++ js/script.js
@@ -70,8 +70,6 @@ function renderGallery(works) {
   empty.classList.add('hidden');
 
   works.forEach(w => {
-    // Skip works without images
-    if (!w.image) return;
     const card = document.createElement('div');
     card.className = 'card';
     const img = document.createElement('img');
